1ced2bc4c66d7f4b82f1261162eb4a2334f1286a
Sai Prasad Paladugu <saiprasadpaladugu@gmail.com>
2022-04-09 15:54:13 -0500

    Update mongoose.js

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
-//Local mongodb://localhost:27017/webAppv3
-//DB Atlas mongodb+srv://expoadmin:expoadmin123@cluster0.lqrpt.mongodb.net/expo-api?retryWrites=true
+/* Local mongodb://localhost:27017/webAppv3
+   DB Atlas mongodb+srv://expoadmin:expoadmin123@cluster0.lqrpt.mongodb.net/expo-api?retryWrites=true */
+
 mongoose.connect(process.env.MDB_CONNECT_URI,{
     useNewUrlParser: true,
     useCreateIndex: true,
